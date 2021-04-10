3b73dc361d322e860ae8e29da7bcab1e529b2bad
LucasMartinSoltz <lucas.soltz@despegar.com>
2021-04-10 17:47:22 -0300

    output respuesta

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,15 @@
 #include <vector>
 using namespace std;
 
-
-
+#define FUERZA_BRUTA "fb"
+#define BACKTRACKING "bt"
+#define BACKTRACKING_FACTIBILIDAD "btf"
+#define BACKTRACKING_OPTIMALIDAD "bto"
+#define PROGRAMACION_DINAMICA "pd"
 
 int main(int argc, char  *argv[]){
 	char *input = argv[1];
+	string algorithm = argv[2];
 
 	int n;
 	int resistance;
@@ -20,31 +24,43 @@ int main(int argc, char  *argv[]){
 
   	inputFile >> n >> resistance;
 
-  	cout << n <<" " << resistance << "\n";
+  	//cout << n <<" " << resistance << "\n";
 
   	for (int i = 0; i < n; i++) {
     	int wi; 
     	int ri;
 
     	inputFile >> wi >> ri;
-    	cout << wi << " " << ri<< "\n";
+    	//cout << wi << " " << ri<< "\n";
     	tuple<int,int> wi_ri (wi,ri);
 
     	tape.push_back(wi_ri);
-  }
+  	}
 
-  int result = FB(tape,resistance,0,0);
-  cout <<"\n" << "Fuerza Bruta: " << result<<"\n" ;
+	if (algorithm == FUERZA_BRUTA) {
+		cout << FB(tape,resistance,0,0) << endl;
+	} else if (algorithm == BACKTRACKING) {
+		cout << BT(tape,resistance,0,0) << endl;
+	} else if (algorithm == BACKTRACKING_FACTIBILIDAD) {
+		cout << BTF(tape,resistance,0,0) << endl;
+	} else if (algorithm == BACKTRACKING_OPTIMALIDAD) {
+		cout << BTO(tape,resistance,0,0) << endl;
+	} else {
+		cout << "IMPLEMENTAR" << endl;
+	}
+	
+	
+/*
+  	int result = FB(tape,resistance,0,0);
+  	cout <<"\n" << "Fuerza Bruta: " << result<<"\n" ;
 
-  int result2 = BTF(tape,resistance,0,0);
-  cout <<"\n" << "Backtracking por factibilidad: "<<  result2<<"\n" ;
+  	int result2 = BTF(tape,resistance,0,0);
+  	cout <<"\n" << "Backtracking por factibilidad: "<<  result2<<"\n" ;
 
-  int result3 = BTO(tape,resistance,0,0);
-  cout <<"\n" << "Backtracking por optimalidad: "<< result3<<"\n" ;
+  	int result3 = BTO(tape,resistance,0,0);
+  	cout <<"\n" << "Backtracking por optimalidad: "<< result3<<"\n" ;
 
-  int result4 = BT(tape,resistance,0,0);
-  cout <<"\n" << "Backtracking: "<< result4<<"\n" ;
-
-
-
+  	int result4 = BT(tape,resistance,0,0);
+  	cout <<"\n" << "Backtracking: "<< result4<<"\n" ;
+*/
 }
